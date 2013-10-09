756affd8175a83f152386b91a07d5796fc72f726
Chris Cheney <ccheney05@gmail.com>
2013-10-08 22:45:39 -0500

    Corrects line indentations and removes debugging logs

--- assets/scripts/formPost.js
+++ assets/scripts/formPost.js
@@ -1,20 +1,18 @@
 $(function(){
-		event.preventDefault();
+    event.preventDefault();
     $( "#sendStatus" ).click(function() {
         var $form = $('#statusUpdate');
-        console.log($form);
         var serializedData = $form.serialize();
-        console.log(serializedData);
         $.ajax({
             url: "http://www.panthersweat.com/dash/updateStatus.php",
             type: "post",
             data: serializedData,
             success: function(){
                 console.log("success");
-                },
-                error:function(){
-                    console.log("failure");
-                    }
-            });
+            },
+            error:function(){
+                console.log("failure");
+            }
         });
+    });
 });
