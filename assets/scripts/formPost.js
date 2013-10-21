7bc55c3abc6e633f391d536ac923f4cebe120941
Michael Szczepanski <mszczepa@nerdery.com>
2013-10-21 00:24:24 -0500

    Added animation to the success message of the status update

--- assets/scripts/formPost.js
+++ assets/scripts/formPost.js
@@ -1,4 +1,5 @@
 $(function(){
+	$(".confirm").hide();
     event.preventDefault();
     $( "#sendStatus" ).click(function() {
         var $form = $('#statusUpdate');
@@ -10,6 +11,11 @@ $(function(){
             data: serializedData,
             success: function(){
                 console.log("success");
+                $(".confirm").addClass("bounceInDown").show(".confirm");
+                setTimeout(function (){
+					$(".confirm").addClass("bounceOutDown");
+			 	}, 2000);
+			 	$(".confirm").removeClass("bounceOutDown");
             },
             error:function(){
                 console.log("failure");
